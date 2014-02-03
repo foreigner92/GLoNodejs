11372e89d80dadad6c4ba4d87dd7850500fb85a5
Matt Richards <matt@lucidmoon.co.uk>
2014-02-03 15:22:44 +0000

    dist

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -17097,7 +17097,15 @@ angular.module('config', []);
 angular.module('config').constant('config', {
   name: 'TastemakerTools',
   version: '1.0',
-  api: { host: 'http://localhost:3030' }
+  api: {
+    host: function () {
+      if (location.href.match(/localhost/).length) {
+        return 'http://localhost:3030';
+      } else {
+        return 'http://tm-app-staging.cloudapp.net:3030/';
+      }
+    }()
+  }
 });
 'use strict';
 (function (angular) {
